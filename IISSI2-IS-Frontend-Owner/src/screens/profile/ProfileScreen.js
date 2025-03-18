093968bdc9a840aa8664d165779bbe23c8bf6ba1
robservil <robservil@alum.us.es>
2025-03-18 13:06:01 +0100

    Mi primer componente

--- IISSI2-IS-Frontend-Owner/src/screens/profile/ProfileScreen.js
+++ IISSI2-IS-Frontend-Owner/src/screens/profile/ProfileScreen.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import { View, Text } from 'react-native'
+import SystemInfo from '../../components/SystemInfo'
 
 export default function ProfileScreen () {
   return (
         <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
             <Text>Profile</Text>
+            <SystemInfo/>
         </View>
   )
 }
