531056f18b42203cf4756bc06b3283ec2ce9af42
robservil <robservil@alum.us.es>
2025-03-18 12:30:53 +0100

    Preparacion Screens y Navegacion

--- IISSI2-IS-Frontend-Owner/App.js
+++ IISSI2-IS-Frontend-Owner/App.js
@@ -1,20 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import { NavigationContainer } from '@react-navigation/native'
+import * as React from 'react'
+import ControlPanelScreen from './src/screens/controlPanel/ControlPanelScreen'
+import ProfileScreen from './src/screens/profile/ProfileScreen'
+import RestaurantsScreen from './src/screens/restaurants/RestaurantsScreen'
 
-export default function App() {
+const Tab = createBottomTabNavigator()
+
+export default function App () {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen
+          name="Restaurants"
+          component={RestaurantsScreen} />
+        <Tab.Screen
+          name="Control Panel"
+          component={ControlPanelScreen} />
+        <Tab.Screen
+          name="Profile"
+          component={ProfileScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
